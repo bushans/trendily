085079e9bfac9ae3ee25d9a91e761acda7969c11
CVG_me <CVG_me@HYDL2BSRIBHAY.india.convergys.com>
2019-11-02 21:06:16 +0530

    updated stripe keys and requirements

--- recommender_app.py
+++ recommender_app.py
@@ -6,20 +6,20 @@ import stripe
 
 app = Flask(__name__)
 
-#STRIPE_PUBLISHABLE_KEY = 'pk_test_1Hv3MowOs3l0jNrEmHITabcw002vq1NuIp'
-#STRIPE_SECRET_KEY = 'sk_test_DXWufFtOIxTe4ZeXrFxwqEyv00hwFNaNFr'
+STRIPE_PUBLISHABLE_KEY = 'pk_test_1Hv3MowOs3l0jNrEmHITabcw002vq1NuIp'
+STRIPE_SECRET_KEY = 'sk_test_DXWufFtOIxTe4ZeXrFxwqEyv00hwFNaNFr'
 
-stripe_keys = {
- 'secret_key': os.environ['STRIPE_SECRET_KEY'],
-  'publishable_key': os.environ['STRIPE_PUBLISHABLE_KEY']
-}
+#stripe_keys = {
+ #'secret_key': os.environ['STRIPE_SECRET_KEY'],
+  #'publishable_key': os.environ['STRIPE_PUBLISHABLE_KEY']
+#}
 
 #if stripe_keys.get('secret_key') != -1:
     #stripe.api_key = stripe_keys['secret_key']
 #else:
 
-stripe.api_key = stripe_keys['secret_key']
-#stripe.api_key = STRIPE_SECRET_KEY
+#stripe.api_key = stripe_keys['secret_key']
+stripe.api_key = STRIPE_SECRET_KEY
     
 p = Predict()
 
@@ -45,8 +45,7 @@ def index():
             return 'There was an issue adding your task'
 
     else:
-        return render_template('index.html', key=stripe_keys['publishable_key'])   
-        #return render_template('index.html', key=STRIPE_SECRET_KEY)
+        return render_template('index.html', key=STRIPE_PUBLISHABLE_KEY)
         #return render_template('index.html', key=stripe_keys['publishable_key'])
 
 @app.route('/charge', methods=['POST'])
